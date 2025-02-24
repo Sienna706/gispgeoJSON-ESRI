095a404ed5beb209f685f53217ccff5f13732c8b
Sienna <siennasilvest@ufl.edu>
2025-02-24 20:09:58 +0000

    Add initial HTML and JavaScript files for map display using ArcGIS API

---
+++ main.js
@@ -0,0 +1,28 @@
+require([
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/layers/GeoJSONLayer"
+], function(Map, MapView, GeoJSONLayer) {
+
+  // URL to the new GeoJSON file
+  const geojsonUrl = "map.geojson"; // Update this line with the path to your new GeoJSON file
+
+  // Create a GeoJSON layer
+  const geojsonLayer = new GeoJSONLayer({
+    url: geojsonUrl
+  });
+
+  // Create a map and add the GeoJSON layer to it
+  const map = new Map({
+    basemap: "topo-vector",
+    layers: [geojsonLayer]
+  });
+
+  // Create a view and set its container to the div with id "viewDiv"
+  const view = new MapView({
+    container: "viewDiv",
+    map: map,
+    center: [-100.33, 25.69], // Longitude, latitude
+    zoom: 3
+  });
+});
